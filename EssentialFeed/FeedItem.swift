2c3ec4d6e5769f1e82b35232f3b01bd041a70cdb
LjubisaKatana <lj.katana85@gmail.com>
2023-03-21 11:47:48 +0100

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ljubisa Katana on 21.3.23..
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
