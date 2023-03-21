65264ad264492accd8014db9d416e4c2b49fa9a2
LjubisaKatana <lj.katana85@gmail.com>
2023-03-21 12:12:00 +0100

    Create the FeedLoader protocol boundary

---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,22 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Ljubisa Katana on 21.3.23..
+//
+
+import Foundation
+
+enum Result {
+    case success([FeedItem])
+    
+    // I can start with Error type but it can bear risk.
+    // Can unnecessary damage/complicate the current design.
+    // On the other hand error case we don't know and don't need to know yet
+    // all errors this feature will have to handle.
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completions: @escaping() -> Void)
+}
