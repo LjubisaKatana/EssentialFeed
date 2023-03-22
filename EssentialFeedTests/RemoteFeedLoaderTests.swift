a04c42c5d1738039c72b1767c631fb5179bc5362
LjubisaKatana <lj.katana85@gmail.com>
2023-03-22 12:59:13 +0100

    Return tuple from the `makeSUT()` factory method

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -37,17 +37,16 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         // arrange
-        let client = HTTPClientSpy()
-        _ = makeSUT(client: client)
+        let (_, client) = makeSUT()
         
+        // assert
         XCTAssertNil(client.requestURL)
     }
     
     func test_load_requestDataFromURL() {
         // arrange
         let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy()
-        let sut = makeSUT(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         // act
         sut.load()
@@ -56,10 +55,12 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestURL, url)
     }
     
-    // MARK: - Helper
+    // MARK: - Helpers
     
-    private func makeSUT(url: URL = URL(string: "https://a-url.com")!, client: HTTPClient) -> RemoteFeedLoader {
-        return RemoteFeedLoader(url: url, client: client)
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
     }
 }
 
