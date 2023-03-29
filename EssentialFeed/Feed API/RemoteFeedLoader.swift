a4ee383c0550f38149d79215c7d765a62e86a8f1
LjubisaKatana <lj.katana85@gmail.com>
2023-03-29 11:44:30 +0200

    Replace magic number with variable giving extra context

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -69,9 +69,11 @@ private class FeedItemsMapper {
         }
     }
     
+    static var OK_200: Int { return 200 } // Why we use this one instead of static let OK_200: Int = 200
+    
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
         
-        guard response.statusCode == 200 else {
+        guard response.statusCode == OK_200 else {
             throw RemoteFeedLoader.Error.invalidData
         }
         
