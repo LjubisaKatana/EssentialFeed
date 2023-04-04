51f326ce17a31e21b2ca1281514f96863c929e06
LjubisaKatana <lj.katana85@gmail.com>
2023-04-04 13:02:17 +0200

    Create URLSession data task with URL on `URLSessionHTTPClient.get(from: URL)`

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -7,10 +7,48 @@
 
 import XCTest
 
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in
+        
+        }
+    }
+}
+
 final class URLSessionHTTPClientTests: XCTestCase {
 
-  
-
+    func test_getFromURL_createDataTaskWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    // MARK: - Helpers
+    
+    private class URLSessionSpy: URLSession {
+        var receivedURLs = [URL]()
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            return FakeURLSessionsDataTask()
+        }
+        
+//        override init() {}
+    }
+    
+    private class FakeURLSessionsDataTask: URLSessionDataTask {
+//        override init() {}
+    }
 }
 
 
@@ -18,5 +56,9 @@ final class URLSessionHTTPClientTests: XCTestCase {
  - We can have end to end approach but it can be unreliable sync hitting the backend
  - Beside that it can be flaky
  - Also we don't have backend side in this moment
+
+ - First we want to capture URL and we can do that by adding Spy again
+ - We override dataTask method and append urls but function returns URLSessionDataTask and we need some mock implementation where we can provide Fake one as return (FakeURLSessionsDataTask)
+
  */
 
