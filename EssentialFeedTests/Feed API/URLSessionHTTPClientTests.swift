b73361a1e4e24ea04c5488664762abb63428b301
LjubisaKatana <lj.katana85@gmail.com>
2023-04-04 12:15:53 +0200

    Add `URLSessionHTTPClientTests` file  and documentation

---
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,22 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  EssentialFeedTests
+//
+//  Created by Ljubisa Katana on 4.4.23..
+//
+
+import XCTest
+
+final class URLSessionHTTPClientTests: XCTestCase {
+
+  
+
+}
+
+
+/* Documentation
+ - We can have end to end approach but it can be unreliable sync hitting the backend
+ - Beside that it can be flaky
+ - Also we don't have backend side in this moment
+ */
+
