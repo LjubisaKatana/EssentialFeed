b6d75e8378712fa867765156432c6ecec857b2b5
LjubisaKatana <lj.katana85@gmail.com>
2023-04-11 17:38:35 +0200

    Add `EssentialFeedAPIEndToEndTests` target and remove test in parallel

---
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Ljubisa Katana on 11.4.23..
+//
+
+import XCTest
+
+final class EssentialFeedAPIEndToEndTests: XCTestCase {
+
+    func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
+            
+    }
+}
