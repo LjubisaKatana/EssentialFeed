56074f32867dff64689938cdc1de84da3560ca22
LjubisaKatana <lj.katana85@gmail.com>
2023-06-21 19:49:03 +0200

    Add LocalFeedItem data transfer representation to decouple storage frameworks from `FeedItem` data models

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -10,6 +10,23 @@ import Foundation
 public protocol FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
     typealias InsertionCompletion = (Error?) -> Void
+    
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+    func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
+
+/// Mirror the FeedItem model and this is the starting point for module decentralisation.
+/// This technique is called DTO (data transfer object) it's just a data transfer representation of the real model
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
 }
