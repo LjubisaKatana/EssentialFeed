1d299245e6420a7678e4812fa51a5d8de379f3ed
LjubisaKatana <lj.katana85@gmail.com>
2023-06-22 09:16:11 +0200

    Add RemoteFeedItem data transfer representation to decouple the items mapper from `FeedItem` data models

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -7,24 +7,16 @@
 
 import Foundation
 
+internal struct RemoteFeedItem: Decodable  {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
+
 internal final class FeedItemsMapper {
     private struct Root: Decodable {
-        let items: [Item]
-        
-        var feed: [FeedItem] {
-            return items.map { $0.item }
-        }
-    }
-
-    private struct Item: Decodable  {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-        
-        var item: FeedItem {
-            return FeedItem(id: id, description: description, location: location, imageURL: image)
-        }
+        let items: [RemoteFeedItem]
     }
     
     private init() {}
@@ -32,15 +24,14 @@ internal final class FeedItemsMapper {
     // Explanation: "Since we're representing a value type (Int), a class constant and a computed var are equivalent in this context. It's a matter of personal preference."
     private static var OK_200: Int { return 200 } // Why we use this one instead of static let OK_200: Int = 200
     
-    internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+    internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            return .failure(RemoteFeedLoader.Error.invalidData)
+            throw RemoteFeedLoader.Error.invalidData
         }
         
-        return .success(root.feed)
+        return root.items
     }
 }
-
 // Since we have this mapper in own module and we don't want to have it accessible to other modules, we can mark it as `internal` It's by default but it's nice to have it visually
 //
 // Also we don't want this to be subclassed so we should mark it as `final`
