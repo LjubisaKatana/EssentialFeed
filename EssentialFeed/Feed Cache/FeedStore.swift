113271c7d5529c3d6652968e61ae660f5bfcde41
LjubisaKatana <lj.katana85@gmail.com>
2023-06-22 11:41:28 +0200

    Move LocalFeedItem to its own file

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -14,19 +14,3 @@ public protocol FeedStore {
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
     func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
 }
-
-/// Mirror the FeedItem model and this is the starting point for module decentralisation.
-/// This technique is called DTO (data transfer object) it's just a data transfer representation of the real model
-public struct LocalFeedItem: Equatable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let imageURL: URL
-    
-    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
-        self.id = id
-        self.description = description
-        self.location = location
-        self.imageURL = imageURL
-    }
-}
