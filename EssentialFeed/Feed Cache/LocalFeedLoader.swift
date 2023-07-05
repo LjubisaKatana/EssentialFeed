181c07db045241baeaecdcbecfe4edcc1334f91a
LjubisaKatana <lj.katana85@gmail.com>
2023-07-05 17:18:34 +0200

    Load command does not deliver a load result after the instance has been deallocated to prevent unexpected behaviours in client's code

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -15,14 +15,15 @@ public final class LocalFeedLoader {
     public typealias SaveResult = Error?
     public typealias LoadResult = LoadFeedResult
     
-    public init(store: FeedStore, currentDate: @escaping() -> Date) {
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
     }
     
-    public func save(_ feed: [FeedImage], completion: @escaping(SaveResult) -> Void) {
+    public func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
         store.deleteCachedFeed { [weak self] error in
             guard let self = self else { return }
+            
             if let cacheDeletionError = error {
                 completion(cacheDeletionError)
             } else {
@@ -30,19 +31,21 @@ public final class LocalFeedLoader {
             }
         }
     }
-    
-    public func load(completion: @escaping(LoadResult ) -> Void) {
-        store.retrieve { [unowned self] result in
+
+    public func load(completion: @escaping (LoadResult) -> Void) {
+        store.retrieve { [weak self] result in
+            guard let self = self else { return }
+
             switch result {
-            case .failure(let error):
+            case let .failure(error):
                 self.store.deleteCachedFeed { _ in }
                 completion(.failure(error))
-            case let .found(feed, timestamp) where validate(timestamp):
+            case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
+                
             case .found:
                 self.store.deleteCachedFeed { _ in }
                 completion(.success([]))
-                
             case .empty:
                 completion(.success([]))
             }
@@ -57,24 +60,22 @@ public final class LocalFeedLoader {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
             return false
         }
-        
         return currentDate() < maxCacheAge
     }
     
     private func cache(_ feed: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
         store.insert(feed.toLocal(), timestamp: currentDate()) { [weak self] error in
             guard self != nil else { return }
+            
             completion(error)
         }
     }
 }
-
 private extension Array where Element == FeedImage {
     func toLocal() -> [LocalFeedImage] {
         return map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
     }
 }
-
 private extension Array where Element == LocalFeedImage {
     func toModels() -> [FeedImage] {
         return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
