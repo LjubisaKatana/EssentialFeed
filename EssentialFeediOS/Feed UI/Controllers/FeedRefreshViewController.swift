a9484ed7bd9604c52577c102dfbf0f527c6c987d
LjubisaKatana <lj.katana85@gmail.com>
2023-09-07 13:58:51 +0200

    Move `FeedViewModel` to it's own file

--- EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -8,61 +8,9 @@
 import UIKit
 import EssentialFeed
 
-final class FeedViewModel {
-    
-    private let feedLoader: FeedLoader
-    
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-    
-    var onChange: ((FeedViewModel) -> Void)?
-    
-    private enum State {
-        case pending
-        case loading
-        case loaded([FeedImage])
-        case failed
-    }
-    
-    private var state = State.pending {
-        didSet { onChange? (self) }
-    }
-    
-    var isLoading: Bool {
-        switch state {
-        case .loading: return true
-        case .pending, .loaded, .failed: return false
-        }
-    }
-    
-    var feed: [FeedImage]? {
-        switch state {
-        case let .loaded(feed): return feed
-        case .pending, .loading, .failed: return nil
-        }
-    }
-        
-    func loadFeed() {
-        state = .loading
-        feedLoader.load { [weak self] result in
-            if let feed = try? result.get() {
-                self?.state = .loaded(feed)
-            } else {
-                self?.state = .failed
-            }
-        }
-    }
-}
-
-
 final class FeedRefreshViewController: NSObject {
-    
-    private(set) lazy var view: UIRefreshControl = {
-        let view = UIRefreshControl()
-        bind(view)
-        return view
-    }()
+     
+    private(set) lazy var view = binded(UIRefreshControl())
 
     private let viewModel: FeedViewModel
     
@@ -76,7 +24,7 @@ final class FeedRefreshViewController: NSObject {
         viewModel.loadFeed()
     }
     
-    private func bind(_ view: UIRefreshControl) {
+    private func binded(_ view: UIRefreshControl) -> UIRefreshControl {
         viewModel.onChange = { [weak self] viewModel in
             if viewModel.isLoading {
                 self?.view.beginRefreshing()
@@ -89,5 +37,6 @@ final class FeedRefreshViewController: NSObject {
             }
         }
         view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        return view
     }
 }
