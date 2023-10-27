0ae5ad3500c84d418cad4705f6e197fdb6496278
LjubisaKatana <lj.katana85@gmail.com>
2023-10-27 13:17:54 +0200

    Move ErrorView to separate file

---
+++ EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Ljubisa Katana on 27.10.23..
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+     public var message: String?
+}
+
