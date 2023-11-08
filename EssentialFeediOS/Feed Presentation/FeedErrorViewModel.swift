83a756d71b03da19d104c16258e1eff6b7f548dd
LjubisaKatana <lj.katana85@gmail.com>
2023-11-08 16:19:18 +0100

    Move FeedErrorViewModel to separate file

---
+++ EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Ljubisa Katana on 8.11.23..
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
