6018f31fdf1a7d65dcb37dd3f6315a731473365d
LjubisaKatana <lj.katana85@gmail.com>
2023-11-24 10:41:41 +0100

    Move FeedViewModel to separate file

---
+++ EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Ljubisa Katana on 23.11.23..
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
