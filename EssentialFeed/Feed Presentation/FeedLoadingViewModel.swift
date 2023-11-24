4de78421674a557200de8508824438c7709c6c0f
LjubisaKatana <lj.katana85@gmail.com>
2023-11-24 10:42:57 +0100

    Move FeedLoadingViewModel to separate file

---
+++ EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Ljubisa Katana on 24.11.23..
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
