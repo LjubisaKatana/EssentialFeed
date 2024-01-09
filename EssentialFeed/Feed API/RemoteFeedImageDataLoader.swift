bbc4c1da63cd30c6a9fffc798207cd2c566b1278
LjubisaKatana <lj.katana85@gmail.com>
2024-01-09 16:05:34 +0100

    Move `RemoteFeedImageDataLoader` to production

---
+++ EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -0,0 +1,45 @@
+//
+//  RemoteFeedImageDataLoader.swift
+//  EssentialFeed
+//
+//  Created by Ljubisa Katana on 9.1.24..
+//
+
+import Foundation
+
+public final class RemoteFeedImageDataLoader {
+    private let client: HTTPClient
+
+    public init(client: HTTPClient) {
+        self.client = client
+    }
+
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+
+    private struct HTTPTaskWrapper: FeedImageDataLoaderTask {
+        let wrapped: HTTPClientTask
+
+        func cancel() {
+            wrapped.cancel()
+        }
+    }
+
+    @discardableResult
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return HTTPTaskWrapper(wrapped: client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+
+            switch result {
+            case let .success((data, response)):
+                if response.statusCode == 200, !data.isEmpty {
+                    completion(.success(data))
+                } else {
+                    completion(.failure(Error.invalidData))
+                }
+            case let .failure(error): completion(.failure(error))
+            }
+        })
+    }
+}
