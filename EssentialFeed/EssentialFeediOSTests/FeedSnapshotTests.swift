3df01d422b4afe3d4426ce28bdb8ad62ec05954f
LjubisaKatana <lj.katana85@gmail.com>
2024-01-23 15:14:44 +0100

    Move UIImage extension to helpers

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -9,19 +9,6 @@ import XCTest
 import EssentialFeediOS
 @testable import EssentialFeed
 
-extension UIImage {
-    static func make(withColor color: UIColor) -> UIImage {
-        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-        UIGraphicsBeginImageContext(rect.size)
-        let context = UIGraphicsGetCurrentContext()!
-        context.setFillColor(color.cgColor)
-        context.fill(rect)
-        let img = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return img!
-    }
-}
-
 class FeedSnapshotTests: XCTestCase {
 
     func test_emptyFeed() {
