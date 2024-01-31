a5d863eb168852c8331a55c75b90c85178255617
LjubisaKatana <lj.katana85@gmail.com>
2024-01-31 20:05:31 +0100

    Replace `FeedImagePresenter` with `LoadResourcePresenter

--- EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
+++ EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
@@ -13,21 +13,21 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     private let loader: () -> AnyPublisher<Resource, Error>
     private var cancellable: Cancellable?
     var presenter: LoadResourcePresenter<Resource, View>?
-
+    
     init(loader: @escaping () -> AnyPublisher<Resource, Error>) {
         self.loader = loader
     }
-
+    
     func loadResource() {
         presenter?.didStartLoading()
-
+        
         cancellable = loader()
             .dispatchOnMainQueue()
             .sink(
                 receiveCompletion: { [weak self] completion in
                     switch completion {
                     case .finished: break
-
+                        
                     case let .failure(error):
                         self?.presenter?.didFinishLoading(with: error)
                     }
@@ -36,9 +36,19 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
                 })
     }
 }
-
 extension LoadResourcePresentationAdapter: FeedViewControllerDelegate {
     func didRequestFeedRefresh() {
         loadResource()
+    }
+}
+
+extension LoadResourcePresentationAdapter: FeedImageCellControllerDelegate {
+    func didRequestImage() {
+        loadResource()
+    }
+    
+    func didCancelImageRequest() {
+        cancellable?.cancel()
+        cancellable = nil
     }
 }
