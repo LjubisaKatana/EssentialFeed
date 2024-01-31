9160b05d389f01293a9e35fd8010898873173f2b
LjubisaKatana <lj.katana85@gmail.com>
2024-01-31 17:28:51 +0100

    Rename and move `ResourceLoadingView` and ViewModel to `Shared` module

---
+++ EssentialFeed/EssentialFeed/Shared Presentation/ResourceLoadingView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceLoadingView.swift
+//  EssentialFeed
+//
+//  Created by Ljubisa Katana on 24.11.23..
+//
+
+import Foundation
+
+public protocol ResourceLoadingView {
+    func display(_ viewModel: ResourceLoadingViewModel)
+}
---
+++ EssentialFeed/EssentialFeed/Shared Presentation/ResourceLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Ljubisa Katana on 31.1.24..
+//
+
+import Foundation
+
+public struct ResourceLoadingViewModel {
+    public let isLoading: Bool
+}
