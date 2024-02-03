19f7d2759f7ec678ea809f113e80ef92e7c21f43
LjubisaKatana <lj.katana85@gmail.com>
2024-02-03 22:54:10 +0100

    Improve coverage with distinct image data per index

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -10,14 +10,14 @@ import EssentialFeed
 import EssentialFeediOS
 @testable import EssentialApp
 
-class FeedAcceptanceTests: XCTestCase {
+final class FeedAcceptanceTests: XCTestCase {
 
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let feed = launch(httpClient: .online(response), store: .empty)
 
         XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 2)
-        //        XCTAssertEqual(feed.renderedFeedImageData(at: 0), makeImageData())
-        //        XCTAssertEqual(feed.renderedFeedImageData(at: 1), makeImageData())
+//        XCTAssertEqual(feed.renderedFeedImageData(at: 0), makeImageData0())
+//        XCTAssertEqual(feed.renderedFeedImageData(at: 1), makeImageData1())
     }
 
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
@@ -29,8 +29,8 @@ class FeedAcceptanceTests: XCTestCase {
         let offlineFeed = launch(httpClient: .offline, store: sharedStore)
 
         XCTAssertEqual(offlineFeed.numberOfRenderedFeedImageViews(), 2)
-        //        XCTAssertEqual(offlineFeed.renderedFeedImageData(at: 0), makeImageData())
-        //        XCTAssertEqual(offlineFeed.renderedFeedImageData(at: 1), makeImageData())
+//        XCTAssertEqual(offlineFeed.renderedFeedImageData(at: 0), makeImageData0())
+//        XCTAssertEqual(offlineFeed.renderedFeedImageData(at: 1), makeImageData1())
     }
 
     func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
@@ -43,7 +43,6 @@ class FeedAcceptanceTests: XCTestCase {
         let store = InMemoryFeedStore.withExpiredFeedCache
 
         enterBackground(with: store)
-
         XCTAssertNil(store.feedCache, "Expected to delete expired cache")
     }
 
@@ -54,7 +53,6 @@ class FeedAcceptanceTests: XCTestCase {
 
         XCTAssertNotNil(store.feedCache, "Expected to keep non-expired cache")
     }
-
 
     func test_onFeedImageSelection_displaysComments() {
         let comments = showCommentsForFirstImage()
@@ -99,28 +97,26 @@ class FeedAcceptanceTests: XCTestCase {
 
     private func makeData(for url: URL) -> Data {
         switch url.path {
-        case "/image-1", "/image-2":
-            return makeImageData()
+        case "/image-0": return makeImageData0()
+        case "/image-1": return makeImageData1()
 
         case "/essential-feed/v1/feed":
             return makeFeedData()
 
         case "/essential-feed/v1/image/2AB2AE66-A4B7-4A16-B374-51BBAC8DB086/comments":
             return makeCommentsData()
-
         default:
             return Data()
         }
     }
 
-    private func makeImageData() -> Data {
-        return UIImage.make(withColor: .red).pngData()!
-    }
+    private func makeImageData0() -> Data { UIImage.make(withColor: .red).pngData()! }
+    private func makeImageData1() -> Data { UIImage.make(withColor: .green).pngData()! }
 
     private func makeFeedData() -> Data {
         return try! JSONSerialization.data(withJSONObject: ["items": [
-            ["id": "2AB2AE66-A4B7-4A16-B374-51BBAC8DB086", "image": "http://feed.com/image-1"],
-            ["id": "A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A", "image": "http://feed.com/image-2"]
+            ["id": "2AB2AE66-A4B7-4A16-B374-51BBAC8DB086", "image": "http://feed.com/image-0"],
+            ["id": "A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A", "image": "http://feed.com/image-1"]
         ]])
     }
 
@@ -140,5 +136,4 @@ class FeedAcceptanceTests: XCTestCase {
     private func makeCommentMessage() -> String {
         "a message"
     }
-
 }
