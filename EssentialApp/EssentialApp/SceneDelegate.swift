b453b13c45190d6b70b6e8a5fe9e2311418c2812
LjubisaKatana <lj.katana85@gmail.com>
2024-02-04 12:08:26 +0100

    Introduce `NullStore` when we can't create a CoreDataFeedStore instance so in case of error we can return NullStore state

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -19,10 +19,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }()
 
     private lazy var store: FeedStore & FeedImageDataStore = {
-        try! CoreDataFeedStore(
-            storeURL: NSPersistentContainer
-                .defaultDirectoryURL()
-                .appendingPathComponent("feed-store.sqlite"))
+        do {
+            return try CoreDataFeedStore(
+                storeURL: NSPersistentContainer
+                    .defaultDirectoryURL()
+                    .appendingPathComponent("feed-store.sqlite"))
+        } catch {
+            return NullStore()
+        }
     }()
 
     private lazy var localFeedLoader: LocalFeedLoader = {
@@ -107,7 +111,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             { self.makeRemoteLoadMoreLoader(last: last) }
         })
     }
-    
+
     private func makeLocalImageLoaderWithRemoteFallback(url: URL) -> FeedImageDataLoader.Publisher {
         let localImageLoader = LocalFeedImageDataLoader(store: store)
         return localImageLoader
